26dcf6eeee897c167a540bdbc46882b839f95e1a
Arun <arunk7839@gmail.com>
2021-07-14 21:20:30 +0530

    code cleanup

--- src/com/learning/Sum1.java
+++ src/com/learning/Sum1.java
@@ -4,13 +4,11 @@ public class Sum1 {
     public static void main(String[] abc){
         java.util.Scanner scanner=new java.util.Scanner(System.in);
         System.out.println("enter start value");
-
         int i =scanner.nextInt();
         System.out.print("enter end value");
         int j=scanner.nextInt();
         int sum=0;
         while (i<=j){
-
             sum= sum+i;
             i++;
         }
